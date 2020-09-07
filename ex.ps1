e12068404358743ee39d9aa6e175263704f86524
orshohat1 <60155108+orshohat1@users.noreply.github.com>
2020-09-07 20:44:04 +0300

    Delete ex.ps1

--- ex.ps1
+++
@@ -1,9 +0,0 @@
-
-function getWebAppName {
-    param ($name)
-    az group create -l westus -n MyResourceGroup
-    az group wait --created  --resource-group MyResourceGroup
-    az appservice plan create -g MyResourceGroup -n MyPlan
-    az webapp create -g MyResourceGroup -p MyPlan -n $name
-    Write-Output web app name is $name
-}
